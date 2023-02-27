52c014cecd1b79ca4095ba0929218a9fc4bd791c
Lucas <124712928+lucasBorcher@users.noreply.github.com>
2023-02-27 22:01:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 const container = document.querySelector(".container");
 const container2 = document.querySelector(".container2")
 const submit = document.getElementById("submit");
+const divs = document.querySelectorAll(".rating-number");
+const result = document.getElementById("result");
+const fejl = document.getElementById("fejl");
+let selectedDiv = false;
 submit.addEventListener("click", function () {
+  if (selectedDiv) {
     container.style.display = "none";
     container2.style.display = "flex";
     console.log("Clicked!");
+  } else {
+    fejl.textContent = 'You need to select a rating before submitting'
+    console.log("fejl");
+  }
 });
 
-// Get all the div elements with class "myDiv"
-const divs = document.querySelectorAll(".rating-number");
 
-// Loop through each div element and add a click event listener to it
 divs.forEach(function(div) {
   div.addEventListener("click", function() {
-    // Remove the "active" class from all div elements
+
     divs.forEach(function(div) {
       div.classList.remove("active");
     });
 
-    // Add the "active" class to the clicked div element
+   
     this.classList.add("active");
     const value = this.getAttribute("data-value");
     result.textContent = 'You selected ' +value +' out of 5';
+    selectedDiv = true;
 
   });
 });
